d54525a0d6479ff5bce3ee9e321ef1c1edd60bb6
Felipe Navas <felipefnavas@gmail.com>
2022-06-21 00:50:29 -0300

    feat: coin converter with webscraping

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 import puppeteer from "puppeteer";
-const url = "https://epicgames.com/store/pt-BR/free-games";
+import readlineSync from "readline-sync";
 
-const browser = await puppeteer.launch({
-  headless: false,
-});
-const page = await browser.newPage();
-await page.goto(url);
-await page.screenshot({ path: "./screenshots/googleTeste.png" });
-/* clica no botão do jogo grátis */
-// await page.$eval("a[role=link]", (el) => el.href());
-// console.log(await page.$eval("a[role=link]"));
+async function coinConverter() {
+  try {
+    const baseCoin =
+      readlineSync.question("Informe uma moeda base: ") || "dolar";
+    const targetCoin =
+      readlineSync.question("Informe a moeda desejada: ") || "real";
+    const url = `https://www.google.com/search?q=${baseCoin}+para+${targetCoin}&oq=${baseCoin}+para+${targetCoin}&aqs=chrome..69i57j0i512l9.2385j1j7&sourceid=chrome&ie=UTF-8`;
 
-// await browser.close();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1920,
+      height: 1080,
+    });
+
+    await page.goto(url);
+    await page.screenshot({
+      path: `./screenshots/${baseCoin}-${targetCoin}_${new Date().getTime()}.png`,
+    });
+
+    const result = await page.evaluate(() => {
+      return {
+        baseCoin: document.querySelector(".a61j6.vk_gy.vk_sh.Hg3mWc").value,
+      };
+    });
+    console.log(
+      `O valor de 1 ${baseCoin} para ${targetCoin} é: ${result.baseCoin}`
+    );
+    await browser.close();
+  } catch (err) {
+    console.log("Erro ao converter moeda");
+  }
+}
+
+await coinConverter();
